61b16ce149c559670cbd896842536a4dc620a958
aagna1108 <166470744+aagna1108@users.noreply.github.com>
2024-09-25 15:35:34 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./Components/inc/Navbar.js";
+
+function App() {
+  return (
+
+      <div>
+        <Navbar />
+          
+      </div>
+
+  )
+}
+
+export default App;
